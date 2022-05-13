de468766248069ac801cbe68419d48ed69e625ff
Dmitry V. Alekseev <mail@alexeevdv.ru>
2022-05-13 11:16:56 +0300

    Try another project structure

---
+++ include/falltergeist/vfs.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "falltergeist/vfs/DatArchiveDriver.h"
+#include "falltergeist/vfs/MemoryDriver.h"
+#include "falltergeist/vfs/NativeDriver.h"
+#include "falltergeist/vfs/VFS.h"
--- include/falltergeist/vfs/vfs.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "VFS.h"
-#include "NativeDriver.h"
-#include "DatArchiveDriver.h"
-#include "MemoryDriver.h"
